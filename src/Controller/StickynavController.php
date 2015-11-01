2737e19eb7e7c9258f4ed10ddc745b3c48591be9
Mihhail <mihhail.arhipov@gmail.com>
2015-11-01 15:57:59 +0000

    Refactored Controller to use proper dependency injection

--- src/Controller/StickynavController.php
+++ src/Controller/StickynavController.php
@@ -8,12 +8,35 @@
 namespace Drupal\stickynav\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Extension\ThemeHandlerInterface;
 use Drupal\Core\Url;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Returns responses for stickynav module routes.
  */
 class StickynavController extends ControllerBase {
+  protected $theme_handler;
+
+  /**
+   * Constructs a \Drupal\stickynav\Controller\StickynavController object.
+   *
+   * @param \Drupal\Core\Extension\ThemeHandlerInterface $theme_handler
+   * The theme handler.
+   */
+  public function __construct(ThemeHandlerInterface $theme_handler) {
+    $this->theme_handler = $theme_handler;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('theme_handler')
+    );
+  }
+
   /**
    * Lists links to configuration for stickynav per theme.
    *
@@ -22,7 +45,7 @@ class StickynavController extends ControllerBase {
    */
   public function listThemes() {
     $build = [];
-    $themes = \Drupal::service('theme_handler')->listInfo();
+    $themes = $this->theme_handler->listInfo();
     $rows = [];
     foreach ($themes as $name => $theme) {
       $row = [$theme->info['name']];
